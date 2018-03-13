1fe783f7c7903552987cd04b254d53255d414fc3
Sam Gould <sjg326@gmail.com>
2018-03-13 11:04:57 -0700

    built in responsive feedback

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -6,20 +6,44 @@ export class Feedback extends React.Component {
     render() {
         const question = {
             word: "Ambivalent",
-            definition: "having mixed feelings or contradictory ideas about something or someone.",
+            definition: "adj. Having mixed feelings or contradictory ideas about something or someone.",
             rightAnswer: "conflicted",
             shuffledArray: ["unrealistic", "apprehensive", "supportive", "conflicted"],
             prompt: "Because he was nervous about performing in front of a crowd, Jed was ambivalent about entering the singing competition."
+        };
+
+        const correct = true;
+
+        let optionFeedback = []
+        for (let i = 0; i < 4; i++) {
+            if (question.shuffledArray[i] === question.rightAnswer) {
+                optionFeedback[i] = "question-option correct-answer-option";
+            } else {
+                optionFeedback[i] = "question-option wrong-answer-option";
+            }
+        }
+
+        let feedback = (
+            <h3 className="question-feedback-text">Incorrect. The answer was <span className="correct-answer-text">{question.rightAnswer}</span>.</h3>
+        )
+        if (correct) {
+            feedback = (
+                <h3 className="question-feedback-text">Correct! The answer was <span className="correct-answer-text">{question.rightAnswer}</span>.</h3>
+            )
         }
         
         return (
             <div className="feedback">
-                <h2>{question.word}</h2>
+                <h2 className="question-word">{question.word}</h2>
+                <div className="word-definition-container">    
+                    <h3 className="word-definition">{question.definition}</h3>
+                </div>
+                {feedback}
                 <div className="question-options-container">
-                    <div className="question-option">A. {question.shuffledArray[0]}</div>
-                    <div className="question-option">B. {question.shuffledArray[1]}</div>
-                    <div className="question-option">C. {question.shuffledArray[2]}</div>
-                    <div className="question-option">D. {question.shuffledArray[3]}</div>
+                    <div className={optionFeedback[0]}>A. {question.shuffledArray[0]}</div>
+                    <div className={optionFeedback[1]}>B. {question.shuffledArray[1]}</div>
+                    <div className={optionFeedback[2]}>C. {question.shuffledArray[2]}</div>
+                    <div className={optionFeedback[3]}>D. {question.shuffledArray[3]}</div>
                 </div>
             </div>
         )
