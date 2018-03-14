821a1a453d3c1451a22b30357983892fd302cf93
Sam Gould <sjg326@gmail.com>
2018-03-14 14:36:49 -0700

    styling for log out button on desktop version

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -43,7 +43,7 @@ export class HeaderBar extends React.Component {
 
         if (this.props.loggedIn) {
             logInLogOut = (
-                <button onClick={() => this.logOut()}>Log out</button>
+                <button className="log-out-button" onClick={() => this.logOut()}>Log out</button>
             );
             faqs = (
                 <div></div>
