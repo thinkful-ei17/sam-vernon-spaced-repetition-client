43f4cea7035e7b730863f180212c92f8c6a2fca2
Sam Gould <sjg326@gmail.com>
2018-03-14 13:36:35 -0700

    finished functionality for only displaying word sets the user has not tried and modify messages based on availability of new word sets

--- src/components/word-sets-dashboard.js
+++ src/components/word-sets-dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
-import './question.css';
+import './word-sets-dashboard.css';
 
 export class WordSetsDashboard extends React.Component {
     render() {
 
+        let welcomeMessage;
+        let newWordSetsMessage;
         let usersWordSets;
         let newWordSets = (
             <div>Loading...</div>
@@ -14,31 +16,64 @@ export class WordSetsDashboard extends React.Component {
         if (this.props.wordSets !== null) {
 
             usersWordSets = this.props.usersWordSets;
+            newWordSets = this.props.wordSets;
 
             usersWordSets = usersWordSets.map( wordSet => {
+                for (let i = 0; i < newWordSets.length; i++) {
+                    if (wordSet.name === newWordSets[i].name) {
+                        newWordSets.splice(i, 1);
+                        break;
+                    }
+                }
+                
                 return (
-                    <div className="wordSet-container">
+                    <div className="wordSet-container" key={wordSet.id}>
                         <button className="wordSet-button" value={wordSet.name} onClick={event => console.log(event.target.value)}>{wordSet.name}</button>
                         <div>Progress: {wordSet.mastery}</div>
                     </div>
                 )
             });
 
-            newWordSets = this.props.wordSets;
-
-            newWordSets = newWordSets.map( wordSet => {
-                return (
-                    <div className="wordSet-container">
-                        <button className="wordSet-button" value={wordSet.name} onClick={event => console.log(event.target.value)}>{wordSet.name}</button>
-                        {/* <div>{wordSet.description}</div> */}
+            
+            if (usersWordSets !== null) {
+                welcomeMessage = (
+                    <div>
+                        <h2>Welcome Back to SATutor!</h2>
+                        <h3>Returned to Master a Word Set?</h3>
                     </div>
                 )
-            });
+            }
+
+            newWordSetsMessage = (
+                <h3>Want to Try a New Word Set?</h3>
+            )
+            
+            if (!newWordSets[0]) {
+                newWordSetsMessage = (
+                    <div className="hide"></div>
+                )
+                newWordSets = (
+                    <div className="hide"></div>
+                )
+            } else {
+                newWordSets = newWordSets.map( wordSet => {
+                    return (
+                        <div className="wordSet-container" key={wordSet.id}>
+                            <button className="wordSet-button" value={wordSet.name} onClick={event => console.log(event.target.value)}>{wordSet.name}</button>
+                            {/* <div>{wordSet.description}</div> */}
+                        </div>
+                    )
+                });
+            }
+
+            
         }
 
         return (
             <div className="wordSets">
+                {welcomeMessage}
                 {usersWordSets}
+                {newWordSetsMessage}
                 {newWordSets}
             </div>
         )
