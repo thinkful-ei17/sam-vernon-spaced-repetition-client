f0f4e3e9b5a48cd79b85e13443c0eb20b445c9c9
Sam Gould <sjg326@gmail.com>
2018-04-16 10:16:43 -0700

    low priority code clean up for actions, reducers, and store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
-import {loadAuthToken} from './local-storage';
+import { loadAuthToken } from './local-storage';
 import { reducer as authReducer } from './reducers/auth';
 import { reducer as usersReducer } from './reducers/users';
 import { reducer as questionReducer } from './reducers/question';
-import {setAuthToken, refreshAuthToken} from './actions/auth';
+import { setAuthToken, refreshAuthToken } from './actions/auth';
 
 const store = createStore(
     combineReducers({
